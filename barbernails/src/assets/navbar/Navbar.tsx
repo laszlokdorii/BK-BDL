cafa3ba855a4a4e39ae7b5be41bcf20550c8722d
Moha0170 <mohalaja06@gmail.com>
2024-04-26 09:16:35 +0200

    normalis navbar css-el, kesobb tovabb designeolni

--- barbernails/src/assets/navbar/Navbar.tsx
+++ barbernails/src/assets/navbar/Navbar.tsx
@@ -2,7 +2,16 @@ import './Navbar.css'
 
 function Navbar() {
     return (
-        <div className="navbar">a</div>
+        <div className="navbar">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
